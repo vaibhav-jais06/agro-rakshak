6803ba2d79fc70d06eba16a8aabaa5455bd57a95
vaibhav-jais06 <vaibhav-jais06@users.noreply.github.com>
2026-08-19 21:20:43 +0530

    fix(cors): allow preview vercel urls

--- backend/server.js
+++ backend/server.js
@@ -36,7 +36,7 @@ const corsOptions = {
       allowedOrigins = ['http://localhost:3000', 'http://localhost:3001', 'https://agro-rakshak.vercel.app'];
     }
     
-    if (allowedOrigins.indexOf(origin) !== -1) {
+    if (allowedOrigins.indexOf(origin) !== -1 || (origin && origin.endsWith('vercel.app'))) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
